4e9b4facf64cd5330c88f273181d5efd430cd7f1
gabrielecossutta <cossuttagabriele@gmail.com>
2024-11-12 22:26:19 +0100

    TextCorrection

--- Source/VampireSurvival/CullingSystem.cpp
+++ Source/VampireSurvival/CullingSystem.cpp
@@ -1,5 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
+//------------------------------------------------------------------Reusable Culling System------------------------------------------------------------------
 
 #include "CullingSystem.h"
 #include "CullingComponent.h"
